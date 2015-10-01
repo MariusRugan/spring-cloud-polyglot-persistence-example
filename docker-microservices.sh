ea3df540e8110fa94c96fbfe32e6c53bf5ee7040
mariusrugan <mariusrugan@gmail.com>
2015-10-01 13:37:45 +0200

    Movie-Microservice update. - Disabled @EnableZuulProxy - Removed basepath - Added docker --link

--- docker-microservices.sh
+++ docker-microservices.sh
@@ -1,21 +1,36 @@
 #!/usr/bin/env bash
+
 #
 # @author mariusrugan@gmail.com
 #
 
 case "$1" in
     discovery)
-        APP="kbastani/discovery-microservice"
+        APP_IMAGE="kbastani/discovery-microservice"
         APP_CONTAINER_NAME="docker_discovery_1"
-        APP_PORTS="-p 8761:8761"
+        APP_CONTAINER_PORTS="-p 8761:8761"
+        APP_CONTAINER_LINKS=""
         ;;
     config)
-        APP="kbastani/config-microservice"
+        APP_IMAGE="kbastani/config-microservice"
         APP_CONTAINER_NAME="docker_config_1"
-        APP_PORTS="-p 8443:8443 -p 5006:5006"
+        APP_CONTAINER_PORTS="-p 8443:8443 -p 5006:5006"
+        APP_CONTAINER_LINKS="--link docker_discovery_1:discovery"
+        ;;
+    movie)
+        APP_IMAGE="kbastani/movie-microservice"
+        APP_CONTAINER_NAME="docker_movie_1"
+        APP_CONTAINER_PORTS="-p 8444:8443 -p 5007:5006"
+        APP_CONTAINER_LINKS="--link docker_discovery_1:discovery --link docker_config_1:config"
+        ;;
+    user)
+        APP_IMAGE="kbastani/user-microservice"
+        APP_CONTAINER_NAME="docker_user_1"
+        APP_CONTAINER_PORTS="-p 8445:8443 -p 5008:5006"
+        APP_CONTAINER_LINKS="--link docker_discovery_1:discovery --link docker_config_1:config"
         ;;
     *)
-        echo "Usage: $0 {discovery|config} {start|stop|restart|status}"
+        echo "Usage: $0 {discovery|config|movie} {start|stop|restart|status}"
         exit 1
         ;;
 esac
@@ -27,24 +42,29 @@ case "$2" in
         cd
         ;;
     start)
-        started=`docker ps | grep ${APP}`
+        CONTAINER_STARTED=`docker ps | grep ${APP_IMAGE} | grep -v Exit`
         CONTAINER_ID=$(docker ps -a | grep -v Exit | grep ${APP_CONTAINER_NAME} | awk '{print $1}')
 
-        if [[ ${started} ]]; then
-            echo "ERROR: ${APP} is up!"
+        if [[ ${CONTAINER_STARTED} ]]; then
+            echo "ERROR: ${APP_IMAGE} is up!"
         else
-            docker run ${APP_PORTS} --hostname=${APP_CONTAINER_NAME} --name=${APP_CONTAINER_NAME} -i -t ${APP}
+            docker run ${APP_CONTAINER_PORTS} --hostname=${APP_CONTAINER_NAME} --name=${APP_CONTAINER_NAME} ${APP_CONTAINER_LINKS} -i -t ${APP_IMAGE}
         fi
         ;;
     stop)
+        CONTAINER_STARTED=`docker ps | grep ${APP_IMAGE} | grep -v Exit`
         CONTAINER_ID=$(docker ps -a | grep ${APP_CONTAINER_NAME} | awk '{print $1}')
-        docker stop ${CONTAINER_ID}
+
+        if [[ ${CONTAINER_STARTED} ]]; then
+            docker stop ${CONTAINER_ID}
+        fi
+
         docker rm -f ${CONTAINER_ID}
         ;;
     restart)
         ;;
     status)
-            docker ps -a | grep ${APP}
+            docker ps -a | grep ${APP_IMAGE}
         ;;
     shell)
             docker exec -i -t ${APP_CONTAINER_NAME} bash
--- movie-microservice/src/main/java/service/Application.java
+++ movie-microservice/src/main/java/service/Application.java
@@ -3,11 +3,11 @@ package service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
 import service.data.domain.Movie;
 import service.data.domain.Genre;
 
@@ -15,8 +15,7 @@ import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "service.config", "service.data" })
-@EnableZuulProxy
-@EnableCircuitBreaker
+@EnableDiscoveryClient
 public class Application {
 
     @Autowired
@@ -29,7 +28,7 @@ public class Application {
     @PostConstruct
     public void postConstructConfiguration() {
         RepositoryRestConfiguration configuration = repositoryRestConfiguration.config();
-        configuration.setBasePath("/api");
+        //configuration.setBasePath("/api");
         configuration.exposeIdsFor(Movie.class);
         configuration.exposeIdsFor(Genre.class);
     }
